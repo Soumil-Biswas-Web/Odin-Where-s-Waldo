7c527a733b25f8ba8f8cd94e565bfa9a8ea0eae9
Soumil Biswas <soumil.biswas19@gmail.com>
2025-01-20 10:20:43 +0530

    Fixed server not listening to public interface 0.0.0.0

--- backend/server.js
+++ backend/server.js
@@ -47,6 +47,6 @@ app.get("/", (req, res) => {
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server started at http://localhost:${PORT}`)
 })
